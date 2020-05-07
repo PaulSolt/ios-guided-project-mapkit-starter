34bae9151fe747ae35a98ee063c77da6c989b3bd
Paul Solt <PaulSolt@gmail.com>
2020-05-07 14:02:16 -0400

    Added callout and custom colors for magnitude severity.

--- Quakes/Quakes/EarthquakesViewController.swift
+++ Quakes/Quakes/EarthquakesViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 import MapKit
 
 class EarthquakesViewController: UIViewController {
-		
+    
     let quakeFetcher = QuakeFetcher()
     
-	// NOTE: You need to import MapKit to link to MKMapView
-	@IBOutlet var mapView: MKMapView!
-	
+    // NOTE: You need to import MapKit to link to MKMapView
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
-		super.viewDidLoad()
-		
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "QuakeView")
+        
         quakeFetcher.fetchQuakes { (quakes, error) in
             
             if let error = error {
@@ -31,10 +34,47 @@ class EarthquakesViewController: UIViewController {
             DispatchQueue.main.async {
                 self.mapView.addAnnotations(quakes)
                 
+                guard let quake = quakes.first else { return }
                 
+                let span = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
+                let region = MKCoordinateRegion(center: quake.coordinate, span: span)
+                self.mapView.setRegion(region, animated: true)
                 
-                
+                // Future: can call a function to do all this work
             }
         }
-	}
+    }
+}
+
+extension EarthquakesViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard let quake = annotation as? Quake else {
+            fatalError("Only quakes are supported")
+        }
+        
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "QuakeView", for: annotation) as? MKMarkerAnnotationView else {
+            fatalError("Missing a registered view")
+        }
+        
+        annotationView.glyphImage = UIImage(named: "QuakeIcon")
+        if let magnitude = quake.magnitude {
+            if magnitude >= 5.0 {
+                annotationView.markerTintColor = .red
+            } else if magnitude >= 3.0 && magnitude < 5.0 {
+                annotationView.markerTintColor = .orange
+            } else {
+                annotationView.markerTintColor = .yellow
+            }
+        } else {
+            annotationView.markerTintColor = .white
+        }
+        
+        annotationView.canShowCallout = true
+        let detailView = QuakeDetailView()
+        detailView.quake = quake
+        annotationView.detailCalloutAccessoryView = detailView
+        
+        return annotationView
+    }
 }
