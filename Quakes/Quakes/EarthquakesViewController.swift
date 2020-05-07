696248a20a40d55afbd442c194653f408df76d3a
Paul Solt <PaulSolt@gmail.com>
2020-05-07 13:35:26 -0400

    Added earthquakes to the map!

--- Quakes/Quakes/EarthquakesViewController.swift
+++ Quakes/Quakes/EarthquakesViewController.swift
@@ -27,6 +27,14 @@ class EarthquakesViewController: UIViewController {
             guard let quakes = quakes else { return }
             
             print("Quakes: \(quakes.count)")
+            
+            DispatchQueue.main.async {
+                self.mapView.addAnnotations(quakes)
+                
+                
+                
+                
+            }
         }
 	}
 }
